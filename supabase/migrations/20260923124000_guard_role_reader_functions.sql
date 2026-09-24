5f509667bece9fe09f0b47be56de7ebd8479ff74
Nikhil jangir <nikhil23@iitk.ac.in>
2026-09-24 06:15:09 +0530

    probe mutant: role.read no longer admitted

--- supabase/migrations/20260923124000_guard_role_reader_functions.sql
+++ supabase/migrations/20260923124000_guard_role_reader_functions.sql
@@ -73,7 +73,7 @@ CREATE FUNCTION "public"."can_read_user_roles"("p_user_id" "uuid")
     SELECT CASE
         WHEN p_user_id = (SELECT auth.uid()) THEN true
         WHEN (SELECT auth.jwt() ->> 'role') = 'service_role' THEN true
-        ELSE COALESCE(public.authorize('role.read'), false)
+        ELSE false
     END;
 $$;
 
